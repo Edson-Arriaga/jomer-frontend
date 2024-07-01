1e9b1ef20c0ae60dc629df8bad674fed76e1a592
Edson Arriaga <edson.arriaga.20@gmail.com>
2024-07-01 12:08:19 -0600

    Add social media links and refactor the layout logic

---
+++ src/components/LinksSocialMedia.tsx
@@ -0,0 +1,20 @@
+export default function LinksSocialMedia() {
+  return (
+    <div className="flex justify-center items-center lg:h-full">
+        <a 
+            href="https://www.instagram.com/jomer.mx/" 
+            target='_blank'
+            className='w-11 h-11 hover:scale-105 ease transition-transform'
+        >
+            <img src="/instagram-icon.svg" alt="Instagram logo"/>
+        </a>
+        <a 
+            href="https://www.facebook.com/Jomeroficial/"
+            target='_blank'
+            className='w-11 h-11 hover:scale-105 ease transition-transform mb-1'
+        >
+            <img src="/facebook-icon.svg" alt="Instagram logo"/>
+        </a>
+    </div>
+  )
+}
