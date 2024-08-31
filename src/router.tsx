65bf1402d5446ba55388ab131326323172d0b4f0
Edson Arriaga <edson.arriaga.20@gmail.com>
2024-08-31 10:05:05 -0600

    Fix routes

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,3 @@
-import { lazy } from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import AppLayout from "./layouts/AppLayout"
 import Home from "./views/Home"
@@ -7,15 +6,13 @@ import AboutUs from "./views/AboutUs"
 import Contact from "./views/Contact"
 import WishList from "./views/WishList"
 import PieceDetails from "./views/PieceDetails"
-
-const Thanks = lazy(() => import("./views/Thanks"))
-const AddPiece = lazy(() => import("./views/admin/AddPiece"))
-const AdminLogin = lazy(() => import("./views/admin/AdminLogin"))
-const AdminLayout = lazy(() => import("./layouts/AdminLayout"))
-const AdminDashboard = lazy(() => import("./views/admin/AdminDashboard"))
-const UpdatePiece = lazy(() => import("./views/admin/UpdatePiece"))
-const NotFound = lazy(() => import("./views/NotFound"))
-
+import UpdatePiece from "./views/admin/UpdatePiece"
+import AddPiece from "./views/admin/AddPiece"
+import AdminDashboard from "./views/admin/AdminDashboard"
+import NotFound from "./views/NotFound"
+import Thanks from "./views/Thanks"
+import AdminLayout from "./layouts/AdminLayout"
+import AdminLogin from "./views/admin/AdminLogin"
 
 export default function Router(){
     return (
